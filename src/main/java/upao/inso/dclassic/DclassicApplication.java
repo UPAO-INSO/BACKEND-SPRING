30f3c55c0d501b89a3365fbfb57fd70c6f88632d
Gerson Portal <gportallinares21@gmail.com>
2025-09-05 00:44:23 -0500

    enable cors

--- src/main/java/upao/inso/dclassic/DclassicApplication.java
+++ src/main/java/upao/inso/dclassic/DclassicApplication.java
@@ -3,7 +3,11 @@ package upao.inso.dclassic;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.lang.NonNullApi;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableJpaAuditing

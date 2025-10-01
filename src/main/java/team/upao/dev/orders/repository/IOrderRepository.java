318f159b0be4edfb1504947bb4763cb7eb968daa
Gerson Portal <gportallinares21@gmail.com>
2025-10-01 01:28:56 -0500

    feat: new endpoint for filter-orders by tableIds and OrderStatus

--- src/main/java/team/upao/dev/orders/repository/IOrderRepository.java
+++ src/main/java/team/upao/dev/orders/repository/IOrderRepository.java
@@ -14,5 +14,6 @@ import java.util.Optional;
 public interface IOrderRepository extends JpaRepository<OrderModel, Long> {
     Page<OrderModel> findAllByOrderStatus(Pageable pageable, OrderStatus status);
     Page<OrderModel> findAllByOrderStatusIn(Pageable pageable, List<OrderStatus> orderStatuses);
+    Page<OrderModel> findAllByTableIdInAndOrderStatusIn(Pageable pageable, List<Long> tableIds, List<OrderStatus> orderStatuses);
     Optional<List<OrderModel>> findByTableIdIn(List<Long> tableIds);
 }

e09186f7786caff3de1f9c50f8f37f1fb9238127
Gerson Portal <gportallinares21@gmail.com>
2025-10-09 17:25:38 -0500

    feat: add jobTitle atribute for UserResponseDto

--- src/main/java/team/upao/dev/users/dto/UserResponseDto.java
+++ src/main/java/team/upao/dev/users/dto/UserResponseDto.java
@@ -2,6 +2,7 @@ package team.upao.dev.users.dto;
 
 import lombok.Builder;
 import lombok.Data;
+import team.upao.dev.jobs.enums.JobEnum;
 import team.upao.dev.users.enums.UserRole;
 
 @Builder
@@ -18,4 +19,6 @@ public class UserResponseDto {
     private Boolean isActive;
 
     private UserRole role;
+
+    private JobEnum jobTitle;
 }

2f660202340560d2bad48395c7ec9cc5c9f7c468
gosthdev <augustusdm8@gmail.com>
2025-12-03 00:20:32 -0500

    fix:products add missing constructors to DTOs for JSON deserialization

--- src/main/java/team/upao/dev/products/dto/ProductRequestDto.java
+++ src/main/java/team/upao/dev/products/dto/ProductRequestDto.java
@@ -3,12 +3,16 @@ package team.upao.dev.products.dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter @Setter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProductRequestDto {
     @NotBlank(message = "Name is required")
     private String name;

9e471217e1efae44be62389fe60efa2fbaa873bc
gosthdev <augustusdm8@gmail.com>
2025-12-04 06:38:03 -0500

    feat: stock validation on orders, initialQuantity for category

--- src/main/java/team/upao/dev/products/dto/ProductRequestDto.java
+++ src/main/java/team/upao/dev/products/dto/ProductRequestDto.java
@@ -1,5 +1,7 @@
 package team.upao.dev.products.dto;
 
+import java.math.BigDecimal;
+
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
@@ -8,8 +10,6 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import java.math.BigDecimal;
 
 @Getter @Setter
 @Builder

b0566ec07ad8134e5c560fbb9bbf73ed13cf4555
Gerson Portal <gportallinares21@gmail.com>
2026-06-07 04:22:45 -0500

    fix(cors): allow Railway frontend origin

--- src/main/java/team/upao/dev/config/SecurityConfig.java
+++ src/main/java/team/upao/dev/config/SecurityConfig.java
@@ -79,7 +79,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource(){
         CorsConfiguration configuration= new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200", "https://punto-de-sal.netlify.app"));
+        configuration.setAllowedOrigins(List.of("http://localhost:4200", "https://punto-de-sal.netlify.app", "https://punto-de-sal.up.railway.app"));
         configuration.setAllowedMethods(List.of("GET","POST","PUT","DELETE", "PATCH", "OPTIONS"));
         configuration.setAllowCredentials(true);
         configuration.setAllowedHeaders(List.of(
